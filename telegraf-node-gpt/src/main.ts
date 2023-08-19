e0e4d435ffe0872e5219c338119c9d0fe7e26b74
Aleksey-devback <bolshoy.enot@mail.ru>
2023-08-20 00:47:19 +0800

    openai handler voice and text

--- telegraf-node-gpt/src/main.ts
+++ telegraf-node-gpt/src/main.ts
@@ -1,27 +1,79 @@
-import { Telegraf } from 'telegraf'
+import { Telegraf, session } from 'telegraf'
 import { message } from 'telegraf/filters'
+import { code } from 'telegraf/format'
 
 import config from 'config'
 
 import { ogg } from './ogg.js'
+import { openAiTg } from './openai.js'
+import { ChatCompletionMessage } from 'openai/resources/chat/index'
+
+const INITIAL_SESSION = {
+    messages: [],
+}
 
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
 
+bot.use(session())
+
+bot.command('new', async ctx => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    ctx.session = INITIAL_SESSION
+    await ctx.reply('Жду вашего голосового или текстового сообщения')
+})
+
+bot.command('start', async ctx => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    ctx.session = INITIAL_SESSION
+    await ctx.reply('Жду вашего голосового или текстового сообщения')
+})
+
 bot.on(message('voice'), async ctx => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    ctx.session ??= INITIAL_SESSION
     try {
+        await ctx.reply(code('Обработка сообщения сервером...'))
         const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
         const userId = String(ctx.message.from.id)
+
         const oggPath = (await ogg.create(link.href, userId)) as string
-        const mp3Path = await ogg.toMp3(oggPath, userId)
-        await ctx.reply(mp3Path as string)
+        const mp3Path = (await ogg.toMp3(oggPath, userId)) as string
+
+        const text = (await openAiTg.transcription(mp3Path)) as string
+
+        await ctx.reply(code(`Ваш запрос ${text}`))
+
+        ctx.session.messages.push({ role: openAiTg.roles.USER, content: text })
+        const response = (await openAiTg.chat(messages)) as ChatCompletionMessage
+        ctx.session.messages.push({ role: openAiTg.roles.ASSISTANT, content: response.content })
+        await ctx.reply(response.content as string)
     } catch (e) {
         const error = e as Error
         console.log('Error while voice message', error.message)
     }
-    await ctx.reply(JSON.stringify(ctx.message.voice))
 })
-bot.command('start', async ctx => {
-    await ctx.reply(JSON.stringify(ctx.message, null, 2))
+
+bot.on(message('text'), async ctx => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    ctx.session ??= INITIAL_SESSION
+    try {
+        await ctx.reply(code('Обработка сообщения сервером...'))
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        ctx.session.messages.push({ role: openAiTg.roles.USER, content: ctx.message.text })
+        const response = (await openAiTg.chat(ctx.session.messages)) as ChatCompletionMessage
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        ctx.session.messages.push({ role: openAiTg.roles.ASSISTANT, content: response.content })
+        await ctx.reply(response.content as string)
+    } catch (e) {
+        const error = e as Error
+        console.log('Error while voice message', error.message)
+    }
 })
 
 await bot.launch()
--- telegraf-node-gpt/src/openai.ts
+++ telegraf-node-gpt/src/openai.ts
@@ -1,8 +1,41 @@
-class OpenAI {
-    constructor() {}
+import OpenAI from 'openai'
+import config from 'config'
+import { createReadStream } from 'fs'
+import { CreateChatCompletionRequestMessage } from 'openai/resources/chat/index'
+class OpenAITg {
+    roles = {
+        ASSISTANT: 'assistant',
+        USER: 'user',
+        SYSTEM: 'system',
+    }
+    openai
+    constructor(apiKey: string) {
+        this.openai = new OpenAI({
+            apiKey,
+        })
+    }
 
-    chat() {}
+    async chat(messages: Array<CreateChatCompletionRequestMessage>) {
+        try {
+            const response = await this.openai.chat.completions.create({ model: 'gpt-4', messages })
+            return response.choices[0].message
+        } catch (e) {
+            const error = e as Error
+            console.log('Error while chat', error.message)
+        }
+    }
 
-    transcription() {}
+    async transcription(filePath: string) {
+        try {
+            const response = await this.openai.audio.transcriptions.create({
+                file: createReadStream(filePath),
+                model: 'whisper-1',
+            })
+            return response.text
+        } catch (e) {
+            const error = e as Error
+            console.log('Error while transcription', error.message)
+        }
+    }
 }
-export const openAi = new OpenAI()
+export const openAiTg = new OpenAITg(config.get('OPENAI_TOKEN'))
