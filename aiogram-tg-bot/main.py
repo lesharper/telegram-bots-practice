3bbdcfee193bf0a4b884725ec1481e975f9a11e1
Aleksey-devback <bolshoy.enot@mail.ru>
2023-09-19 02:28:35 +0800

    aiogram magic filter

--- aiogram-tg-bot/main.py
+++ aiogram-tg-bot/main.py
@@ -11,8 +11,21 @@ async def cmd_start(message: Message):
     await message.answer('Добро пожаловать')
 
 
+@dp.message(F.text == '/my_info')
+async def cmd_my_id(message: Message):
+    await message.answer(f'Твой ID: {message.from_user.id}')
+    await message.answer(f'Твое имя: {message.from_user.first_name}')
+    await message.answer(f'Твой никнейм: {message.from_user.username}')
+
+
+@dp.message()
+async def echo(message: Message):
+    await message.answer(message.text)
+
+
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
