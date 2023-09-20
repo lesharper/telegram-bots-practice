90f3ded98c1b7dd9755801a38f394914c9e3f89f
Aleksey-devback <bolshoy.enot@mail.ru>
2023-09-20 21:54:06 +0800

    photo,document handlers

--- aiogram-tg-bot/main.py
+++ aiogram-tg-bot/main.py
@@ -18,6 +18,30 @@ async def cmd_my_id(message: Message):
     await message.answer(f'Твой никнейм: {message.from_user.username}')
 
 
+@dp.message(F.text == '/send_image')
+async def cmd_send_imahe(message: Message):
+    await message.answer_photo(photo='https://i.pinimg.com/564x/a1/88/70/a18870d05cd698d05aea26557ce75eda.jpg',
+                               caption='Изображение с интернета')
+    await message.answer_photo(
+        photo='AgACAgIAAxkBAAOVZQr3_Kf4hOioVQcs8q0369UCRMAAAmDKMRuAbFlI3D4nWpghHAIBAAMCAAN5AAMwBA',
+        caption='Изображение по ID')
+
+
+@dp.message(F.text == '/send_doc')
+async def cmd_send_doc(message: Message):
+    await message.answer_document(document="")
+
+
+@dp.message(F.photo)
+async def cmd_get_photo_id(message: Message):
+    await message.answer(message.photo[-1].file_id)
+
+
+@dp.message(F.document)
+async def cmd_get_doc_id(message: Message):
+    await message.answer(message.document.file_id)
+
+
 @dp.message()
 async def echo(message: Message):
     await message.answer(message.text)
